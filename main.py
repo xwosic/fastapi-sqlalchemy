6d21ade31a19997711ba65fd891493495969c16b
michal_cisowski <cisowski.mc@pg.com>
2021-12-30 14:37:02 +0100

    add get pets endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from db_setup import setup_db
-from models import User
+from models import User, Pet
 from dependencies import get_session
 
 app = FastAPI()
@@ -12,10 +12,20 @@ def setup_database():
 
 
 @app.get('/users')
-def get_all_users(session = Depends(get_session)):
+def get_all_users(session=Depends(get_session)):
     return session.query(User).all()
 
 
 @app.get('/users/{user_id}')
-def get_user(user_id: int, session = Depends(get_session)):
+def get_user(user_id: int, session=Depends(get_session)):
     return session.query(User).filter(User.id==user_id).all()
+
+
+@app.get('/pets')
+def get_all_pets(session=Depends(get_session)):
+    return session.query(Pet).all()
+
+
+@app.get('/pets/{pet_id}')
+def get_pet(pet_id: int, session=Depends(get_session)):
+    return session.query(Pet).filter(Pet.id==pet_id).all()
