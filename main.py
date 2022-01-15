1a7e231d929e4d6913b03dde973cb398d239e446
xwosic <mcisow@gmail.com>
2022-01-15 16:26:48 +0100

    delete user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.param_functions import Query
+from fastapi.param_functions import Path
 from db_setup import setup_db
 from models import User, Pet
 from dependencies import get_session
@@ -30,7 +30,7 @@ def get_users(params: GetUserParams = Depends(),
     return query.all()
 
 
-@app.put('/users')
+@app.put('/users/update')
 def update_user(params: UpdateUserParams,
                 session=Depends(get_session)):
     user = session.query(User).filter(User.id==params.id).first()
@@ -43,6 +43,17 @@ def update_user(params: UpdateUserParams,
     session.commit()
     return 200
 
+
+@app.delete('/users/{id}/delete')
+def delete_user(id: int = Path(...),
+                session=Depends(get_session)):
+    user_to_delete = session.query(User).filter(User.id==id).first()
+    if user_to_delete:
+        session.delete(user_to_delete)
+        session.commit()
+
+    return 200
+    
 
 @app.put('/users/new')
 def new_user(params: InsertUserParams,
